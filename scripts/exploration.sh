1385c36dfae4ff30af3784a9ecdff83b17d83d18
RuslanAgishev <agishrus@fel.cvut.cz>
2024-01-12 16:56:30 +0100

    separate launcher for exploration with traversability estimation: lss or hm from lidar cloud

--- scripts/exploration.sh
+++ scripts/exploration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# source the workspace and launch the exploration
 ws=/opt/ros/navigation_ws
 source $ws/devel/setup.bash
-roslaunch robingas_mission_gazebo exploration.launch record:=true rviz:=false
+roslaunch robingas_mission_gazebo exploration_lss.launch record:=false lss:=false rviz:=true
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #  run the exploration with naex in the singularity container
-singularity run --nv --bind ../:/opt/ros/navigation_ws/src/robingas_mission_gazebo/ \
+singularity run --nv \
+    --bind ../:/opt/ros/navigation_ws/src/robingas_mission_gazebo/ \
+    --bind $HOME/workspaces/traversability_ws/src/monoforce/:/opt/ros/navigation_ws/src/monoforce/ \
     ../singularity/robingas_mission_gazebo.simg \
     ../scripts/exploration.sh

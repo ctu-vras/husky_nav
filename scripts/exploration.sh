17e377a3e226399f7b36108965ad3712ea4a7471
RuslanAgishev <agishrus@fel.cvut.cz>
2024-02-10 18:00:57 +0100

    cleaned up, copied path follower from naex

--- scripts/exploration.sh
+++ scripts/exploration.sh
@@ -3,4 +3,4 @@
 # source the workspace and launch the exploration
 ws=/opt/ros/navigation_ws
 source $ws/devel/setup.bash
-roslaunch robingas_mission_gazebo exploration_lss.launch record:=false lss:=false rviz:=true
+roslaunch husky_sim exploration_lss.launch record:=false lss:=false rviz:=true
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 #  run the exploration with naex in the singularity container
 singularity run --nv \
-    --bind ../:/opt/ros/navigation_ws/src/robingas_mission_gazebo/ \
+    --bind ../:/opt/ros/navigation_ws/src/husky_sim/ \
     --bind $HOME/workspaces/traversability_ws/src/monoforce/:/opt/ros/navigation_ws/src/monoforce/ \
-    ../singularity/robingas_mission_gazebo.simg \
+    ../singularity/husky_sim.simg \
     ../scripts/exploration.sh
